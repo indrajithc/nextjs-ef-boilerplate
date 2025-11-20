e9c1c758bc8cb5a3421fe12a6d958b850330593d
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-11-20 22:19:32 +0530

    refactor: test edge

---
+++ api/hello.ts
@@ -0,0 +1,12 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export const config = {
+  runtime: "edge", // this is a pre-requisite
+};
+
+export default function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  return response.status(200).json({ text: "I am an Vercel Function!" });
+}
