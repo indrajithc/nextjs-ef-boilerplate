1b6b4c99f9b6920965b961192a28e3afb9c80faf
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-11-20 16:04:58 +0530

    refactor: stall time added

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,4 +1,16 @@
+
+// function to stall for n time
+function stall(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export async function GET(request: Request) {
+  // read query parameter named t from request url 
+  const url = new URL(request.url);
+  const t = url.searchParams.get('t');
+  if (t) {
+    await stall(parseInt(t));
+  }
   const response = await fetch('https://api.vercel.app/products');
   const products = await response.json();
   return Response.json(products);
