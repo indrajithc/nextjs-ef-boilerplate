989e74712125c7bae4ead739613434f9ba2ad0c8
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2025-11-20 16:09:04 +0530

    refactor: simple logic to test

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -8,10 +8,16 @@ export async function GET(request: Request) {
   // read query parameter named t from request url 
   const url = new URL(request.url);
   const t = url.searchParams.get('t');
+  const requestTime = new Date().toISOString();
   if (t) {
+    console.log(`Stalling for ${t} ms`);
     await stall(parseInt(t));
   }
-  const response = await fetch('https://api.vercel.app/products');
-  const products = await response.json();
-  return Response.json(products);
+
+  return Response.json({
+    message: 'Hello, Next.js!',
+    requestTime: requestTime,
+    responseTime: new Date().toISOString(),
+    data: { foo: 'bar' }
+  });
 }
